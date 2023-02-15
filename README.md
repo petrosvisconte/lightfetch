d1359c2f0c6aa5832900986af3695635ebd2ec6e
PetrosVisconte <94921305+petrosvisconte@users.noreply.github.com>
2023-02-14 18:03:42 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# lightfetch
+A lightweight system information fetcher for Linux  
+
+![Alt text](/screenshots/lightfetch0.png?raw=true "Optional Title")
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Author: Pierre Visconti
+# Installation script for lightfetch, requires root/sudo privilege
+# This script is intended to be run from within the lightfetch directory pulled from github
+
+set -euo pipefail
+
+if [ -f lightfetch ]; then
+    sudo cp lightfetch /usr/local/bin
+else 
+    echo "> lightfetch file not found"
+    echo "> Installation failed"
+    exit 0
+fi
+
+input=
+until [ "${input}" == "y" ] || [ "${input}" == "n" ]; do
+    read -p "Create a "${HOME}"/.config/lightfetch directory? [y/n]: " input    
+    if [ "${input}" == "y" ]; then
+        mkdir -v "${HOME}"/.config/lightfetch2
+        # copy themes to .config
+        echo "> Installation complete. Try entering: lightfetch"
+    elif [ "${input}" == "n" ]; then
+        echo "> Installation complete. Try entering: lightfetch"
+    fi
+done
+
